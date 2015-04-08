baf8bba1f592dde6965aad295300fcd58ef63af6
jee-pay <jee-pay.parmentier@laposte.net>
2015-04-08 14:20:32 +0200

    Page de Login (Login.php)
    3 variables de sessions renseignés
    $_SESSION['login']
    $_SESSION['idUser']
    $_SESSION['connected']
    User(1).sql pour Julien

--- Login.php
+++ Login.php
@@ -1,18 +1,23 @@
-<?php include ('include/header.html'); ?>
+<?php 
+session_start();
+include ('include/header.html'); 
+?>
 <div id="loginF" class="form">
-    <form method="POST" action="">
-        <label for="login">Login : </label><input type="text" name="login" id="login" value="">
-        <label for="pass">Password : </label><input type="password" name="pass" id="pass" value='' >
-        <input type="submit" value="Login">
+    <form method="POST" action="backoffice.php">
+        <label for="login">Login : </label><input required="required" type="text" name="login" id="login" value="">
+        <label for="pass">Password : </label><input required="required" type="password" name="pass" id="pass" value='' >
+        <input name="logon" type="submit" value="Login">
     </form>
     <a href="#" id="inscription">Pas encore de compte ?</a>
 </div>
 <div id="inscriptionF" class="form">
-    <form method="POST" action="">
-        <label for="loginI">Login : </label><input type="text" name="loginI" id="loginI" value="">
-        <label for="passI">Password : </label><input type="password" name="passI" id="passI" value='' >
-        <label for="emailI">Email : </label><input type="email" name="mail" id="mail">
-        <input type="submit" value="Inscription">
+    <form method="POST" action="backoffice.php">
+        <label for="nom">Nom : </label><input required="required" type="text" name="nom" id="nom"><br>
+        <label for="prenom">Prenom : </label><input required="required" type="text" name="prenom" id="prenom"><br>
+        <label for="motpasse">Password : </label><input required="required" type="password" name="motpasse" id="motpasse" value='' ><br>
+        <label for="verifpasse">Password : </label><input required="required" type="password" name="verifpasse" id="verifpasse" value='' ><br>
+        <!-- <label for="emailI">Email : </label><input required="required" type="email" name="mail" id="mail"> -->
+        <input name="register" type="submit" value="Inscription">
     </form>
     <a href="#" id="login">Deja inscrit ?</a>
 </div>
@@ -24,7 +29,7 @@ $('#inscriptionF').hide();
 
 $(document).on('click','#inscription',function(){
     $('#loginF').hide();
-    $('#inscriptionF').show('fas');
+    $('#inscriptionF').show('fast');
 });
 
 $(document).on('click','#login',function(){
