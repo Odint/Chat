a7a9525213aa4e72c0e84e75c81d3727229be56e
Odint <aymanekhouaji@live.com>
2015-04-08 09:39:09 +0200

    Login

---
+++ Login.php
@@ -0,0 +1,8 @@
+<?php include ('include/header.html'); ?>
+<div id="loginF">
+    <form method="POST" action="">
+        <label>Login : <input type="text" name="login" id="login"></label>
+        <label>Password : <input type="password" name="pass" id="pass" ></label>
+    </form>
+</div>
+<?php include ('include/footer.html'); ?>
