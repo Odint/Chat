afbbdfeb99373453dd98af300c645e4da81e285b
Odint <aymanekhouaji@live.com>
2015-04-08 10:46:50 +0200

    aymane
    formulaire login et inscription et un peu de css

--- Login.php
+++ Login.php
@@ -1,8 +1,35 @@
 <?php include ('include/header.html'); ?>
-<div id="loginF">
+<div id="loginF" class="form">
     <form method="POST" action="">
-        <label>Login : <input type="text" name="login" id="login"></label>
-        <label>Password : <input type="password" name="pass" id="pass" ></label>
+        <label for="login">Login : </label><input type="text" name="login" id="login" value="">
+        <label for="pass">Password : </label><input type="password" name="pass" id="pass" value='' >
+        <input type="submit" value="Login">
     </form>
+    <a href="#" id="inscription">Pas encore de compte ?</a>
 </div>
+<div id="inscriptionF" class="form">
+    <form method="POST" action="">
+        <label for="loginI">Login : </label><input type="text" name="loginI" id="loginI" value="">
+        <label for="passI">Password : </label><input type="password" name="passI" id="passI" value='' >
+        <label for="emailI">Email : </label><input type="email" name="mail" id="mail">
+        <input type="submit" value="Inscription">
+    </form>
+    <a href="#" id="login">Deja inscrit ?</a>
+</div>
+
+
+<script>
+
+$('#inscriptionF').hide();
+
+$(document).on('click','#inscription',function(){
+    $('#loginF').hide();
+    $('#inscriptionF').show('fas');
+});
+
+$(document).on('click','#login',function(){
+    $('#inscriptionF').hide();
+    $('#loginF').show('fast');
+});
+</script>
 <?php include ('include/footer.html'); ?>
